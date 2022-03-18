85cb78702f50217016a4ad0771092e020714e04d
zeldaababil19 <zeldaababil01@gmail.com>
2022-03-19 01:33:49 +0700

    homework

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+const REACT_APP_GIPHY_KEY = process.env.REACT_APP_GIPHY_KEY;
+const REACT_APP_SPOTIFY = process.env.REACT_APP_SPOTIFY;
 
 function App() {
+  useEffect(() => {
+    console.log('giphy client id');
+    console.log(REACT_APP_GIPHY_KEY);
+    console.log('spotify client id');
+    console.log(REACT_APP_SPOTIFY);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,12 +20,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
